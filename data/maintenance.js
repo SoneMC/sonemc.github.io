0fe06adb567ccdc9124d2294c14067e1f7c8fa93
SoneMC <sonemcpl@gmail.com>
2025-06-15 20:18:07 +0200

    Add files via upload

---
+++ data/maintenance.js
@@ -0,0 +1,45 @@
+export const maintenanceConfig = {
+    // Maintenance status
+    status: 'maintenance',
+    
+    // Maintenance information
+    info: {
+        startTime: '2025-06-15T20:35',
+        estimatedEnd: '2025-06-20T21:00:00',
+        reason: 'Project SoneMC2 is under development.',
+        type: 'Active Development',
+    },
+    
+    progress: 15, 
+    
+    // Status messages
+    statusMessages: {
+        maintenance: 'Project is currently under maintenance.',
+        planned: 'Maintenance was planned and is ongoing.',
+        emergency: 'Project had to be set to maintenance due to issues.',
+    },
+    
+    // Additional info
+    infoItems: [
+        {
+            icon: 'fas fa-clock',
+            label: 'Started (UTC+2)',
+            value: 'format-start-time'
+        },
+        {
+            icon: 'fas fa-hourglass-start',
+            label: 'Est. Duration',
+            value: '5 days'
+        },
+        {
+            icon: 'fas fa-tools',
+            label: 'Type',
+            value: 'Scheduled'
+        },
+        {
+            icon: 'fas fa-chart-line',
+            label: 'Progress',
+            value: 'format-progress'
+        }
+    ]
+};
