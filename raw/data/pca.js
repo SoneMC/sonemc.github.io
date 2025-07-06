6acdb48c032c349f0226c8f77207eb122b74283c
SoneMC <sonemcpl@gmail.com>
2025-07-07 00:14:21 +0200

    Update pca.js

--- raw/data/pca.js
+++ raw/data/pca.js
@@ -17,14 +17,14 @@ Please note:
 - These archived projects are provided "as is" without any warranty. SoneMC is not responsible for any damages or issues resulting from their use.
 
 
-Type     | Name           | Archive Link                                  | Ended Development | Archived     
----------|----------------|----------------------------------------------|-------------------|--------------
-Plugin   | Valentines     | https://github.com/SoneMC/Plugin-Code-Archive/tree/main/Valentines  | May 17, 2025      | June 1, 2025 
-Plugin   | LetsPlay       | https://github.com/SoneMC/Plugin-Code-Archive/tree/main/LetsPlay    | May 15, 2025      | June 1, 2025 
-Website  | sonemc         | https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc      | April 19, 2025    | June 1, 2025 
-Website  | sonemc1        | https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc1     | June 10, 2025     | June 1, 2025 
-Website  | sonemc2        | https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc2     | June 27, 2025     | June 1, 2025 
-Website  | sonemc3_summer | https://sonemcpl.pages.dev                                          | MM DD, YYYY       | MM DD, YYYY 
+Type      | Name          | Archive Link                                  | Ended Development | Archived     
+----------|---------------|-----------------------------------------------|-------------------|--------------
+| Plugin  | Valentine     | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/Valentines) | May 17, 2025 | June 1, 2025 |
+| Plugin  | LetsPlay      | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/LetsPlay)   | May 15, 2025 | June 1, 2025 |
+| Website | sonemc        | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc)   | April 19, 2025 | June 1, 2025 |
+| Website | sonemc1       | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc1)   | June 10, 2025 | June 1, 2025 |
+| Website | sonemc2       | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc2)   | June 27, 2025 | June 1, 2025 |
+| Website | sonemc3_summe | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc3_summer)   | July 07, 2025 | July 07, 2025 |
 
 Last updated: July 6, 2025
 `;
