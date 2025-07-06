123e979fd271c487cb6cccbd9285c72bd95a731a
SoneMC <sonemcpl@gmail.com>
2025-07-07 00:54:28 +0200

    Update pca.js

--- raw/data/pca.js
+++ raw/data/pca.js
@@ -17,21 +17,18 @@ Please note:
 - These archived projects are provided "as is" without any warranty. SoneMC is not responsible for any damages or issues resulting from their use.
 
 
-| Type | Name | Archive Link | Ended Development | Archived |
-|------|------|--------------|-------------------|----------|
-| Plugin | Valentines  | [Click](#) | May 17, 2025 | June 1, 2025 |
-| Plugin | LetsPlay    | [Click](#)   | May 15, 2025 | June 1, 2025 |
-| Website | sonemc    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc)   | April 19, 2025 | June 1, 2025 |
-| Website | sonemc1    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc1)   | June 10, 2025 | June 1, 2025 |
-| Website | sonemc2    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc2)   | June 27, 2025 | June 1, 2025 |
-| Website | sonemc3_summer    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc3_summer)   | July 07, 2025 | July 07, 2025 |
-| Plugin | Coinflip    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/plugins/Coinflip_1.0.0)   | July 07, 2025 | July 07, 2025 |
-| Plugin | Minesweeper    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/plugins/Minesweeper_1.0.0)   | July 07, 2025 | July 07, 2025 |
-| Plugin | TicTacToe    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/plugins/TicTacToe_1.0.0)   | July 07, 2025 | July 07, 2025 |
+| Type | Name | Archive Link | Ended Development | Archived | Unarchived |
+|------|------|--------------|-------------------|----------|--------------|
+| Plugin | Valentines  | [Click](#) | May 17, 2025 | June 1, 2025 | Yes (Development will continue in 2026) |
+| Plugin | LetsPlay    | [Click](#)   | May 15, 2025 | June 1, 2025 | No |
+| Website | sonemc    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc)   | April 19, 2025 | June 1, 2025 |  No |
+| Website | sonemc1    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc1)   | June 10, 2025 | June 1, 2025 | No |
+| Website | sonemc2    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc2)   | June 27, 2025 | June 1, 2025 | No |
+| Website | sonemc3_summer    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/sonemc3_summer)   | July 07, 2025 | July 07, 2025 | No |
+| Plugin | Coinflip    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/plugins/Coinflip_1.0.0)   | July 07, 2025 | July 07, 2025 | No |
+| Plugin | Minesweeper    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/plugins/Minesweeper_1.0.0)   | July 07, 2025 | July 07, 2025 | No |
+| Plugin | TicTacToe    | [Click](https://github.com/SoneMC/Plugin-Code-Archive/tree/main/plugins/TicTacToe_1.0.0)   | July 07, 2025 | July 07, 2025 | No |
 
-| Type | Name         | Ended Development | Unarchived | Reason |
-|------|--------------|--------------------|----------|---------|
-| Plugin | Valentines | May 17, 2025 | July 7, 2025 | Development will continue on February 2026 |
 
 Last updated: July 6, 2025
 `;
