baede1792ecef7ce22472dc85d6f5918276d848b
SoneMC <sonemcpl@gmail.com>
2025-07-08 15:07:34 +0200

    Updated plugins.js

--- data/plugins.js
+++ data/plugins.js
@@ -13,11 +13,55 @@ const pluginsData = {
                     date: '2025-07-07',
                     githubUrl: 'https://github.com/SoneMC/SoneRPG/releases/tag/1.0.0-B1_Early',
                     downloadUrl: 'https://github.com/SoneMC/SoneRPG/releases/download/1.0.0-B1_Early/SoneRPG_1.0.0-B1_Early.jar',
+const pluginsData = {
+    plugins: [
+        {
+            id: 'sonerpg',
+            name: 'SoneRPG',
+            description: 'Skyrim-themed (RPG-like) plugin with races, quests, shops, smithing, skills & lot more..',
+            currentVersion: '1.0.0-B1_Early',
+            mcVersions: ['1.20.x'],
+            downloadUrl: 'https://github.com/SoneMC/SoneRPG/releases/download/1.0.0-B1_Early/SoneRPG_1.0.0-B1_Early.jar',
+            changelog: [
+                {
+                    version: '1.0.0-B1_Early',
+                    date: '2025-07-07',
+                    githubUrl: 'https://github.com/SoneMC/SoneRPG/releases/tag/1.0.0-B1_Early',
+                    downloadUrl: 'https://github.com/SoneMC/SoneRPG/releases/download/1.0.0-B1_Early/SoneRPG_1.0.0-B1_Early.jar',
+const pluginsData = {
+    plugins: [
+        {
+            id: 'sonerpg',
+            name: 'SoneRPG',
+            description: 'Skyrim-themed (RPG-like) plugin with races, quests, shops, smithing, skills & lot more..',
+            currentVersion: '1.0.0-B1_Early',
+            mcVersions: ['1.20.x'],
+            downloadUrl: 'https://github.com/SoneMC/SoneRPG/releases/download/1.0.0-B1_Early/SoneRPG_1.0.0-B1_Early.jar',
+            changelog: [
+                {
+                    version: '1.0.0-B1_Early',
+                    date: '2025-07-07',
+                    githubUrl: 'https://github.com/SoneMC/SoneRPG/releases/tag/1.0.0-B1_Early',
+                    downloadUrl: 'https://github.com/SoneMC/SoneRPG/releases/download/1.0.0-B1_Early/SoneRPG_1.0.0-B1_Early.jar',
                     description: `First public version. There may be a lot of problems but on testing, everything was fine.
 
 Keep in mind that from this version, there will be even more updates with more "drastic" changes.
 
 If you want to test this plugin, feel free. If you want to use it, use it, but I recommend to use it for fun/testing and not for public servers.`
+                }
+            ]
+        },
+    ]
+};
+
+window.pluginsData = pluginsData; q
+                }
+            ]
+        },
+    ]
+};
+
+window.pluginsData = pluginsData;u
                 }
             ]
         },
