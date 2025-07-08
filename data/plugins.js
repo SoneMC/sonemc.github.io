b6aba1356f1b25b11c964febb4f9ebbbffd6463d
SoneMC <sonemcpl@gmail.com>
2025-07-08 15:01:34 +0200

    Add multi-line support to changelog descriptions

--- data/plugins.js
+++ data/plugins.js
@@ -13,7 +13,11 @@ const pluginsData = {
                     date: '2025-07-07',
                     githubUrl: 'https://github.com/SoneMC/SoneRPG/releases/tag/1.0.0-B1_Early',
                     downloadUrl: 'https://github.com/SoneMC/SoneRPG/releases/download/1.0.0-B1_Early/SoneRPG_1.0.0-B1_Early.jar',
-                    description: 'First public version. There may be a lot of problems but on testing, everything was fine. Keep in mind that fron this version, there will be even more updates with more "drastic" changes. If you want to test this plugin, feel free. If you want to use it, use it, but I recommend to use it for fun/testing and not for public servers'
+                    description: `First public version. There may be a lot of problems but on testing, everything was fine.
+
+Keep in mind that from this version, there will be even more updates with more "drastic" changes.
+
+If you want to test this plugin, feel free. If you want to use it, use it, but I recommend to use it for fun/testing and not for public servers.`
                 }
             ]
         },
